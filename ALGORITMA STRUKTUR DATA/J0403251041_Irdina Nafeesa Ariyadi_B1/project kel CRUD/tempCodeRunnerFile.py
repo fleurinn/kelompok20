f450695bbf84d91f4e2147d6cd5df7244a8b4b0a
ayyafitriaramadhani <ayyafitria24ramadhani@apps.ipb.ac.id>
2026-04-15 06:04:04 +0700

    Delete ALGORITMA STRUKTUR DATA/J0403251041_Irdina Nafeesa Ariyadi_B1/project kel CRUD/tempCodeRunnerFile.py

--- ALGORITMA STRUKTUR DATA/J0403251041_Irdina Nafeesa Ariyadi_B1/project kel CRUD/tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-    for kategori, daftar_merk in buka_data.items():
-                print(f"\n• {kategori}")
-                for merk, daftar_series in daftar_merk.items():
-                    print(f"   ├── {merk}")
-                    for series in daftar_series:
-                        print(f"   │    • {series}")
