210c4f7103d27956ef63ac0f70ac5a43effb7fb0
fleurinn <adhyass00@gmail.com>
2026-05-13 14:22:17 +0700

    added perbaruan

--- array.py
+++ array.py
@@ -5,111 +5,40 @@
 
 # 1. FILE HANDLING & DICTIONARY
 nama_file = "kategori.txt"  # file berisi kategori produk
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.next = None
-        self.prev = None
-
-
-class DoubleCircularLinkedList:
-    def __init__(self):
-        self.head = None
-
-    def is_empty(self):
-        return self.head is None
-
-    def append(self, data):
-        new_node = Node(data)
-
-        if self.head is None:
-            self.head = new_node
-            new_node.next = new_node
-            new_node.prev = new_node
-        else:
-            tail = self.head.prev
-
-            tail.next = new_node
-            new_node.prev = tail
-
-            new_node.next = self.head
-            self.head.prev = new_node
-
-    def to_list(self):
-        hasil = []
-        if self.head is None:
-            return hasil
-
-        current = self.head
-        while True:
-            hasil.append(current.data)
-            current = current.next
-            if current == self.head:
-                break
-        return hasil
-
-    def delete_at_index(self, index):
-        if self.head is None:
-            return
-
-        current = self.head
-        count = 0
-
-        while True:
-            if count == index:
-                if current.next == current:  # hanya 1 node
-                    self.head = None
-                else:
-                    current.prev.next = current.next
-                    current.next.prev = current.prev
-
-                    if current == self.head:
-                        self.head = current.next
-                return
-
-            current = current.next
-            count += 1
-            if current == self.head:
-                break
-
-    def update_at_index(self, index, data_baru):
-        current = self.head
-        count = 0
-
-        while True:
-            if count == index:
-                current.data = data_baru
-                return
-
-            current = current.next
-            count += 1
-            if current == self.head:
-                break
 
 def muat_data_kategori(nama_file):
-    database_kategori = {}
+    """
+    Fungsi membaca kategori.txt lalu menyimpannya
+    ke dictionary bertingkat
+    Format file: kategori,merk,series
+    """
 
-    try:
-        with open(nama_file, "r", encoding="utf-8") as file:
-            for baris in file:
-                baris = baris.strip()
-                parts = baris.split(",")
+    database_kategori = {}  # dictionary utama
 
-                if len(parts) != 3:
-                    continue
+    # buka file kategori.txt mode baca
+    with open(nama_file, "r", encoding="utf-8") as file:
 
-                kategori, merk, series = [x.strip() for x in parts]
+        # baca file per baris
+        for baris in file:
+            baris = baris.strip()  # hapus enter/newline
+            parts = baris.split(",")  # pisahkan berdasarkan koma
 
-                if kategori not in database_kategori:
-                    database_kategori[kategori] = {}
+            # validasi harus 3 bagian
+            if len(parts) != 3:
+                continue
 
-                if merk not in database_kategori[kategori]:
-                    database_kategori[kategori][merk] = DoubleCircularLinkedList()
+            kategori, merk, series = parts
 
-                database_kategori[kategori][merk].append(series)
+            # jika kategori belum ada, buat dictionary baru
+            if kategori not in database_kategori:
+                database_kategori[kategori] = {}
 
-    except FileNotFoundError:
-        print("File belum ada, akan dibuat saat penyimpanan.")
+            # jika merk belum ada, buat list baru
+            if merk not in database_kategori[kategori]:
+                database_kategori[kategori][merk] = []
+
+            # tambahkan series ke merk
+            database_kategori[kategori][merk].append(series)
 
     return database_kategori
 
@@ -128,7 +57,7 @@ for kategori, daftar_merk in buka_data.items():
     for merk, daftar_series in daftar_merk.items():
         print(f"   ├── {merk}")
 
-        for series in daftar_series.to_list():
+        for series in daftar_series:
             print(f"   │    • {series}")
 
 
@@ -140,39 +69,31 @@ def tambah_data(buka_data):
     merk = input("Masukkan merk: ").strip()
     series = input("Masukkan series: ").strip()
 
-    kategori_key = cari_key_case_insensitive(buka_data, kategori)
-
-    if kategori_key is None:
+    # validasi kategori
+    if kategori not in buka_data:
         print("Kategori belum ada, akan dibuat baru.")
+
+    # buat kategori jika belum ada
+    if kategori not in buka_data:
         buka_data[kategori] = {}
-        kategori_key = kategori
 
-    merk_key = cari_key_case_insensitive(buka_data[kategori_key], merk)
-
-    if merk_key is None:
-        buka_data[kategori_key][merk] = DoubleCircularLinkedList()
-        merk_key = merk
+    # buat merk jika belum ada
+    if merk not in buka_data[kategori]:
+        buka_data[kategori][merk] = []
 
     # cek duplikasi series
-    if series not in buka_data[kategori][merk].to_list():
+    if series not in buka_data[kategori][merk]:
         buka_data[kategori][merk].append(series)
         print("Data berhasil ditambahkan!")
     else:
         print("Series sudah ada!")
-
-#pencarian tanpa case sensitive
-def cari_key_case_insensitive(data_dict, key_input):
-    for key in data_dict.keys():
-        if key.lower() == key_input.lower():
-            return key
-    return None
 
 #menyimpan data
 def simpan_data(nama_file, data):
     with open(nama_file, "w", encoding="utf-8") as file:
         for kategori, merk_dict in data.items():
             for merk, series_list in merk_dict.items():
-                for series in series_list.to_list():
+                for series in series_list:
                     file.write(f"{kategori},{merk},{series}\n")
 
 # update data 
@@ -185,11 +106,7 @@ def update_data(buka_data):
     for i in range(len(daftar_kategori)):
         print(f"{i+1}. {daftar_kategori[i]}")
 
-    try:
-        pilih_kategori = int(input("Pilih kategori: ")) - 1
-    except:
-        print("Input harus angka!")
-        return
+    pilih_kategori = int(input("Pilih kategori: ")) -1
 
     if 0 <= pilih_kategori < len(daftar_kategori):
         kategori_terpilih = daftar_kategori[pilih_kategori]
@@ -200,11 +117,7 @@ def update_data(buka_data):
         for i in range(len(daftar_merk)):
             print(f"{i+1}. {daftar_merk[i]}")
 
-        try:
-            pilih_merk = int(input("Pilih merk: ")) - 1
-        except:
-            print("Input harus angka!")
-            return
+        pilih_merk = int(input("Pilih merk: ")) - 1
 
         if 0 <= pilih_merk < len(daftar_merk):
             merk_terpilih = daftar_merk[pilih_merk]
@@ -212,21 +125,16 @@ def update_data(buka_data):
             daftar_series = buka_data[kategori_terpilih][merk_terpilih]
 
             # tampilkan series 
-            daftar_series = daftar_series.to_list()
             for i in range(len(daftar_series)):
                 print(f"{i+1}. {daftar_series[i]}")
 
-            try:
-                pilih_series = int(input("Pilih series yang ingin diupdate: ")) - 1
-            except:
-                print("Input harus angka!")
-                return
+            pilih_series = int(input("Pilih series yang mau diupdate: ")) - 1
 
             if 0 <= pilih_series < len(daftar_series):
                 series_baru = input("Masukkan nama series baru: ").strip()
 
                 # update data
-                buka_data[kategori_terpilih][merk_terpilih].update_at_index(pilih_series, series_baru)
+                buka_data[kategori_terpilih][merk_terpilih][pilih_series] = series_baru
                 print("Data berhasil diupdate")
             else:
                 print("Pilihan series tidak valid")
@@ -318,7 +226,6 @@ def hapus_data(buka_data):
                 merk = daftar_merk[pilih_merk]
                 daftar_series = buka_data[kategori][merk]
 
-                daftar_series = daftar_series.to_list()
                 for i in range(len(daftar_series)):
                     print(f"{i+1}. {daftar_series[i]}")
 
@@ -329,7 +236,7 @@ def hapus_data(buka_data):
 
                     konfirmasi = input(f"Yakin hapus series '{series}'? (y/t): ").lower()
                     if konfirmasi == "y":
-                        buka_data[kategori][merk].delete_at_index(pilih_series)
+                        del buka_data[kategori][merk][pilih_series]
                         print("Series berhasil dihapus!")
                     else:
                         print("Dibatalkan")
@@ -342,26 +249,6 @@ def hapus_data(buka_data):
     else:
         print("Pilihan tidak valid")
 
-def tampil_next_prev(series_dcll):
-    if series_dcll.head is None:
-        print("Kosong")
-        return
-
-    current = series_dcll.head
-
-    while True:
-        print(f"\nSekarang: {current.data}")
-        print(f"Prev: {current.prev.data}")
-        print(f"Next: {current.next.data}")
-
-        aksi = input("n=next, p=prev, q=keluar: ")
-
-        if aksi == "n":
-            current = current.next
-        elif aksi == "p":
-            current = current.prev
-        elif aksi == "q":
-            break
 # ==========================================================
 # MAIN PROGRAM
 # ==========================================================
@@ -385,10 +272,9 @@ def main():
             while True:
                 print("\n--- SUB MENU KATEGORI ---")
                 print("1. Pilih Kategori")
-                print("2. Lihat Next/Prev Series")
-                print("3. Kembali")
+                print("2. Kembali")
 
-                sub_pilihan = input("Pilih submenu (1-3): ").strip()
+                sub_pilihan = input("Pilih submenu (1-2): ").strip()
 
                 if sub_pilihan == "1":
                     daftar_kategori = list(buka_data.keys())
@@ -414,36 +300,14 @@ def main():
                             merk_terpilih = daftar_merk[pilih_merk]
 
                             print(f"\nSeries {merk_terpilih}:")
-                            for series in buka_data[kategori_terpilih][merk_terpilih].to_list():
+                            for series in buka_data[kategori_terpilih][merk_terpilih]:
                                 print(f"• {series}")
                         else:
                             print("Pilihan merk tidak valid")
                     else:
                         print("Pilihan kategori tidak valid")
+
                 elif sub_pilihan == "2":
-                    daftar_kategori = list(buka_data.keys())
-
-                    for i in range(len(daftar_kategori)):
-                        print(f"{i+1}. {daftar_kategori[i]}")
-
-                    pilih_kategori = int(input("Pilih kategori: ")) - 1
-
-                    if 0 <= pilih_kategori < len(daftar_kategori):
-                        kategori = daftar_kategori[pilih_kategori]
-
-                        daftar_merk = list(buka_data[kategori].keys())
-
-                        for i in range(len(daftar_merk)):
-                            print(f"{i+1}. {daftar_merk[i]}")
-
-                        pilih_merk = int(input("Pilih merk: ")) - 1
-
-                        if 0 <= pilih_merk < len(daftar_merk):
-                            merk = daftar_merk[pilih_merk]
-
-                            # 🔥 INI INTINYA
-                            tampil_next_prev(buka_data[kategori][merk])
-                elif sub_pilihan == "3":
                     break
 
                 else:
